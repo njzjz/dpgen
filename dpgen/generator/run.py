5dee62548b5c5a3a1e85e32ae3d338e98e9be4b5
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-07-06 17:29:12 -0400

    support pairwise_dprc parameters (#116)
    * support pairwise_dprc parameters
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- dpgen/generator/run.py
+++ dpgen/generator/run.py
@@ -538,7 +538,14 @@ def make_train(iter_index, jdata, mdata):
             mdata["deepmd_version"]
         ) < Version("3"):
             # 1.x
-            if jinput["model"]["descriptor"]["type"] == "hybrid":
+            if jinput["model"].get("type") == "pairwise_dprc":
+                jinput["model"]["qm_model"]["descriptor"]["seed"] = random.randrange(
+                    sys.maxsize
+                ) % (2**32)
+                jinput["model"]["qmmm_model"]["descriptor"]["seed"] = random.randrange(
+                    sys.maxsize
+                ) % (2**32)
+            elif jinput["model"]["descriptor"]["type"] == "hybrid":
                 for desc in jinput["model"]["descriptor"]["list"]:
                     desc["seed"] = random.randrange(sys.maxsize) % (2**32)
             elif jinput["model"]["descriptor"]["type"] == "loc_frame":
@@ -547,9 +554,17 @@ def make_train(iter_index, jdata, mdata):
                 jinput["model"]["descriptor"]["seed"] = random.randrange(
                     sys.maxsize
                 ) % (2**32)
-            jinput["model"]["fitting_net"]["seed"] = random.randrange(sys.maxsize) % (
-                2**32
-            )
+            if jinput["model"].get("type") == "pairwise_dprc":
+                jinput["model"]["qm_model"]["fitting_net"]["seed"] = random.randrange(
+                    sys.maxsize
+                ) % (2**32)
+                jinput["model"]["qmmm_model"]["fitting_net"]["seed"] = random.randrange(
+                    sys.maxsize
+                ) % (2**32)
+            else:
+                jinput["model"]["fitting_net"]["seed"] = random.randrange(
+                    sys.maxsize
+                ) % (2**32)
             if "type_embedding" in jinput["model"]:
                 jinput["model"]["type_embedding"]["seed"] = random.randrange(
                     sys.maxsize
